af4cb93ddcb0cb8054b44d478dee5ca80773e293
Th3Unkn0wn <kristian.tashkov@gmail.com>
2013-04-03 22:54:37 +0300

    Empty new clojure project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject game-of-life-the-game "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main game-of-life-the-game.core)
