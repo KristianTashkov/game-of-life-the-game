390033854dacbcebc7f62bb849a24619a8dff9d1
Nikola Taushanov <nktaushanov@gmail.com>
2013-04-05 01:52:36 +0300

    Fix identation.

--- src/game/server.clj
+++ src/game/server.clj
@@ -7,15 +7,15 @@
 (defn logger-write
   [message]
   (binding [*out* standart-out]
-   (println message)
-   (flush)))
+    (println message)
+    (flush)))
 
 (defn read-message
   []
   (let [message (try (let [msg (read-line)]
                        (read-line)
                        msg)
-                     (catch Exception e (logger-write "Socket faulted state.")))]
+                  (catch Exception e (logger-write "Socket faulted state.")))]
     (if-not (nil? message)
       (clojure.string/trim-newline message)
       message)))
@@ -24,15 +24,15 @@
   (binding [*in* (reader in)
             *out* (writer out)
             *err* (writer System/err)]
-      (loop [input (read-message)]
-        (when-not (nil? input)
-                (logger-write input)
-                (if (= input "joke")
-                  (println "Haha thats funny!")
-                  (println (str "Why did you say " input " to me?!")))
-                (flush)
-                (when-not (nil? *in*)
-                  (recur (read-message)))))
+    (loop [input (read-message)]
+      (when-not (nil? input)
+        (logger-write input)
+        (if (= input "joke")
+          (println "Haha thats funny!")
+          (println (str "Why did you say " input " to me?!")))
+        (flush)
+        (when-not (nil? *in*)
+          (recur (read-message)))))
     (logger-write "Client exiting...")))
 
 (defn start-server [server-info] (create-server (Integer. (:port server-info)) client-callback))
